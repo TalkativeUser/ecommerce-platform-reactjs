7ff4e7e9d2efda1755534a45ca9a9034c07b49cb
TalkativeUser <morshedy480@gmail.com>
2026-03-29 06:23:30 +0200

    add products skeleton

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -69,11 +69,11 @@ useEffect(() => {
 
 
         {/* Loading Spinner */}
-        {loading ? (
-          <div className="flex flex-col items-center justify-center py-24">
-            <div className="w-12 h-12 border-4 border-primary-200 border-t-primary-600 rounded-full animate-spin mb-4" />
-            <p className="text-gray-500 text-sm">Loading products...</p>
-          </div>
+        {loading ? ( <ProductsSkeleton/>
+          // <div className="flex flex-col items-center justify-center py-24">
+          //   <div className="w-12 h-12 border-4 border-primary-200 border-t-primary-600 rounded-full animate-spin mb-4" />
+          //   <p className="text-gray-500 text-sm">Loading products...</p>
+          // </div>
         ) : (
           <>
             {/* Product Grid */}
@@ -142,3 +142,40 @@ useEffect(() => {
     </ProductsLayout>
   );
 }
+
+
+const ProductsSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      {Array.from({ length: 8 }).map((_, index) => (
+        <div 
+          key={index} 
+          className="relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-full animate-pulse"
+        >
+         
+          <div className="relative grid h-56 mx-4 mt-4 overflow-hidden text-gray-700 bg-gray-300 bg-clip-border rounded-xl place-items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" className="w-12 h-12 text-gray-500">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"></path>
+            </svg>
+          </div>
+
+        
+          <div className="p-6">
+            <div className="block w-56 h-3 mb-4 bg-gray-300 rounded-full">&nbsp;</div>
+            <div className="block w-full h-2 mb-2 bg-gray-300 rounded-full">&nbsp;</div>
+            <div className="block w-full h-2 mb-2 bg-gray-300 rounded-full">&nbsp;</div>
+            <div className="block w-full h-2 mb-2 bg-gray-300 rounded-full">&nbsp;</div>
+            <div className="block w-full h-2 mb-2 bg-gray-300 rounded-full">&nbsp;</div>
+          </div>
+
+          {/* زر التفاعل */}
+          <div className="p-6 pt-0 flex justify-between items-center ">
+            <div className=" h-2 w-14 bg-gray-300 rounded-lg">&nbsp;</div>
+            <div className="h-8 w-20 bg-gray-300 rounded-lg">&nbsp;</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
